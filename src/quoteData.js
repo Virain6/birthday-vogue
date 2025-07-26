e09b3867d6ddf74bdd9fa0110c833458fcb6696d
Virain6 <97552281+Virain6@users.noreply.github.com>
2025-07-26 04:12:12 -0400

    Fixed text size problem

--- src/quoteData.js
+++ src/quoteData.js
@@ -1,8 +1,16 @@
 // src/quoteData.js
 
 export const quoteLayers = [
-  ["Dream big.", "Shine bright.", "Stay bold."],
-  ["Make today amazing.", "Glow up.", "Keep rising."],
-  ["Live loud.", "You're iconic.", "Sparkle on."],
-  ["Another year bolder.", "Make it count.", "Joy mode: ON"],
+  ["Dream big. - Sukhmun", "Shine bright. - Sukhmun", "Stay bold. - Sukhmun"],
+  [
+    "Make today amazing. - Sukhmun",
+    "Glow up. - Sukhmun",
+    "Keep rising. - Sukhmun",
+  ],
+  ["Live loud. - Sukhmun", "You're iconic. - Sukhmun", "Sparkle on. - Sukhmun"],
+  [
+    "Another year bolder. - Sukhmun",
+    "Make it count. - Sukhmun",
+    "Joy mode: ON - Sukhmun",
+  ],
 ];
